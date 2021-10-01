a106916859363ce9b38bc68b18a09442dc5d6cf9
Quentin Williams <quentinbwilliams@gmail.com>
2021-10-01 12:37:43 -0400

    fixed numerous typos in test-data

--- queries.sql
+++ queries.sql
@@ -1,11 +1,13 @@
+-- GET ALL USERS AND THE LEAGUES THEY FOLLOW
 SELECT * FROM users AS u
 JOIN users_leagues AS ul
 ON u.id = ul.user_id 
 JOIN leagues as l
 ON ul.league_id = l.id;
 
-SELECT * FROM users WHERE id = 1
+-- GET TEAMS FOR USER WITH ID
+SELECT * FROM users WHERE id = $1
 JOIN users_teams AS ut
 ON users.id = ut.user_id 
 JOIN teams as t
-ON ut.team_id = t.id;
+ON ut.team_id = t.id

ddfb45829d0cdcab5bae6e08dc13aa9120aaee49
Quentin Williams <quentinbwilliams@gmail.com>
2021-09-30 22:39:19 -0400

    add user routes for favorite leagues and teams

--- routes/user.js
+++ routes/user.js
@@ -4,27 +4,68 @@ const ExpressError = require("../error");
 const router = express.Router();
 
 function getUser(id) {
-  const userRes = await db.query(`SELECT name, type FROM users WHERE id = $1`, [
-    id,
-  ]);
-  // check if user exists
-  if (userRes.rows.length === 0) {
-    throw new ExpressError("User not found", 404);
-  }
-  const user = userRes.rows[0];
-  return user;
+	const userRes = await db.query(`SELECT name, type FROM users WHERE id = $1`, [
+		id,
+	]);
+	// check if user exists
+	if (userRes.rows.length === 0) {
+		throw new ExpressError("User not found", 404);
+	}
+	const user = userRes.rows[0];
+	return user;
 }
 
 // GET user by id
 router.get("/:id", async (req, res, next) => {
+	const {
+		id
+	} = req.params;
+	try {
+		user = getUser(id)
+		return res.send(user);
+	} catch (err) {
+		return next(err);
+	}
+});
+
+
+// GET user leagues
+router.get("/:id", async (req, res, next) => {
+	const {
+		id
+	} = req.params;
+	try {
+		const usersLeagues = await db.query(
+			`SELECT * FROM users WHERE id = $1
+			JOIN users_leagues AS ul
+			ON users.id = ul.user_id 
+			JOIN leagues as l
+			ON ul.league_id = l.id`, [id]
+		)
+		return usersLeagues
+	} catch (e) {
+		return next(e);
+	}
+})
+
+// GET user teams
+router.get("/:id", async (req, res, next) => {
   const { id } = req.params;
   try {
-    user = getUser(id)
-    return res.send(user);
-  } catch (err) {
-    return next(err);
+    const usersTeams = await db.query(
+      `SELECT * FROM users WHERE id = $1
+			JOIN users_teamss AS ut
+			ON users.id = ut.user_id 
+			JOIN leagues as l
+			ON ut.league_id = l.id`,
+      [id]
+    );
+    return usersTeams;
+  } catch (e) {
+    return next(e);
   }
 });
+
 
 // GET user favorite teams
 // router.get("/:id", async (req, res, next) => {

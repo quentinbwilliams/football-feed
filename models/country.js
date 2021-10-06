0d607e17a79cd7c884bcae66d59a629e06682cc4
Quentin Williams <quentinbwilliams@gmail.com>
2021-10-06 18:12:07 -0400

    refactored country model

--- models/country.js
+++ models/country.js
@@ -1,51 +1,61 @@
-axios = require("axios").default;
+const axios = require("axios").default;
 const db = require("../db/db");
 const headers = require("../headers/api-football");
 
 class Country {
-  constructor(name, code) {
-    this.name = name;
-    this.code = code;
-  }
+	constructor(code, name) {
+		this.code = code;
+		this.name = name;
+	}
 
-  static async getCountryWithCode(countryCode) {
-    // RETURN COUNTRY ROW FROM DB
-    const query = await db.query(
-      `SELECT name, code, flag
-			FROM countries
-			WHERE code = $1`,
-      [countryCode]
-    );
-    return query;
-  }
+	static async getCountries() {
+		const options = {
+			headers: headers
+		}
+		const request = await axios.get(
+			"https://api-football-v1.p.rapidapi.com/v3/countries",
+			options
+		);
+		const countries = request.data.response;
+		return countries;
+	}
 
-  static async getCountries() {
-    const request = await axios.get(
-      "https://api-football-v1.p.rapidapi.com/v3/countries",
-      headers
-    );
-    const countries = request.data.response;
-    return countries;
-  }
+	async initCountryData() {
+		const options = {
+			params: {
+				code: this.code
+			},
+			headers: headers,
+		};
+		const request = await axios.get(
+			"https://api-football-v1.p.rapidapi.com/v3/countries",
+			options
+		);
+		const info = request.data.response;
+		this.info = info;
+	}
 
-  async createCountryRow(next) {
-    try {
-      const insert = await db.query(
-        `INSERT INTO countries (name, code, flag) VALUES ($1,$2,$3) RETURNING name, code, flag`,
-        [this.name, this.code, this.flag]
-      );
-    } catch (e) {
-      return next(e);
-    }
-  }
+	async insertCountryData(next) {
+		for (let i = 0; i < this.info.length; i++) {
+			try {
+				const insert = await db.query(
+					`INSERT INTO countries (name, code, flag) VALUES ($1,$2,$3) RETURNING name, code, flag`,
+					[this.info[0].name, this.info[0].code, this.info[0].flag]
+				);
+			} catch (e) {
+				return next(e);
+			}
+		}
+	}
 
-  getLeagues() {
-    // CALL API TO OBTAIN LEAGUES & UPDATE VALUE OF LEAGUES ON COUNTRY INSTANCE
-  }
 
-  getNationalTeam() {
-    // CALL API FOR INFO ON COUNTRIES NATIONAL TEAMS
-  }
+	getLeagues() {
+		// CALL API TO OBTAIN LEAGUES & UPDATE VALUE OF LEAGUES ON COUNTRY INSTANCE
+	}
+
+	getNationalTeam() {
+		// CALL API FOR INFO ON COUNTRIES NATIONAL TEAMS
+	}
 }
 
 module.exports = Country;

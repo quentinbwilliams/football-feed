e24871246f91837ef2647fce619a48b1ea1e2cd8
Quentin Williams <quentinbwilliams@gmail.com>
2021-11-03 11:23:15 -0400

    seed/matches should update rows not drop table

--- models/team.js
+++ models/team.js
@@ -172,7 +172,7 @@ class Team {
 			const query = await db.query(
 				`SELECT api_football_id, league, league_id, season, round, date, referee, home, home_id, away, away_id, ht_home, ht_away, ft_home, ft_away, et_home, et_away, pen_home, pen_away, home_win, away_win,  created_at
 				FROM matches
-				WHERE home_id = $1 OR away_id = $1 AND ft_home >= 0`,
+				WHERE home_id = $1 AND ft_home >= OR away_id = $1 AND ft_home >= 0`,
 				[this.apiFootballID]
 			);
 			const data = query.rows;

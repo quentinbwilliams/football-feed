52f607786a8d20460ddc75baa8c19f206c3afba6
Quentin Williams <quentinbwilliams@gmail.com>
2021-11-05 11:18:29 -0400

    create LeagueTeam class; add method to create leagues_teams view; create script to call method

---
+++ models/league-team.js
@@ -0,0 +1,25 @@
+const db = require("../db/db");
+const season = require("../season");
+const headers = require("../headers/api-football");
+const axios = require("axios").default;
+const ExpressError = require("../error");
+
+class LeagueTeam {
+	constructor(leagueID, teamID) {
+		this.leagueID = leagueID;
+		this.teamID = teamID;
+	}
+
+	static async dbCreateLeaguesTeamsTable() {
+		try {
+			const create = await db.query(
+				`CREATE MATERIALIZED VIEW leagues_teams AS SELECT DISTINCT league_id, home_id FROM matches;`
+			);
+			return create.rows;
+		} catch (e) {
+			return new ExpressError(e);
+		}
+	}
+}
+
+module.exports = LeagueTeam

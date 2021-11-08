984aced143bf702165d7991626c3de5ab6d7f583
Quentin Williams <quentinbwilliams@gmail.com>
2021-11-08 10:45:28 -0500

    update cron

--- db/seed/cron.js
+++ db/seed/cron.js
@@ -3,24 +3,26 @@ const updateMatches = require("./matches");
 const updateLeaguesTeams = require("./leagues-teams");
 const ExpressError = require("../../error");
 const LEAGUE_IDS = require("../../leagues-array");
+const currentDate = new Date().toLocaleString();
 
 // UPDATE matches table at 8am, 2pm, 8pm EST
-const matches = cron.schedule("* * 8,14,20 * *", async () => {
+const matches = cron.schedule("* * 8,14,20 * * *", async () => {
 	try {
+		const currentDate = new Date().toLocaleString();
 		const update = await updateMatches(LEAGUE_IDS);
-		console.log(`CRON UPDATED MATCHES AT ${new Date().toLocaleString()}`);
+		console.log(`CRON UPDATED MATCHES AT ${currentDate}`);
 	} catch (e) {
 		return new ExpressError(e);
 	}
 });
 
-// UPDATE leagues_teams view at midnight each night
-const leaguesTeams = cron.schedule("* * 0 * *", async () => {
+// UPDATE leagues_teams 10 minutes after updating matches
+const leaguesTeams = cron.schedule("* 10 8,14,20 * * *", async () => {
 	try {
+		const currentDate = new Date().toLocaleString();
+
 		const update = await updateLeaguesTeams();
-		console.log(
-			`CRON UPDATED LEAGUES TEAMS VIEW AT ${new Date().toLocaleString()}`
-		);
+		console.log(`CRON UPDATED LEAGUES TEAMS VIEW AT ${currentDate}`);
 	} catch (e) {
 		return new ExpressError(e);
 	}

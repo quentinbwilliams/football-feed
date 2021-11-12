5144cfc5d900192ce3a1d86eec78655a25ae420f
Quentin Williams <quentinbwilliams@gmail.com>
2021-11-12 10:23:13 -0500

    update cron with wc qualifiers

--- db/seed/cron.js
+++ db/seed/cron.js
@@ -1,11 +1,12 @@
 const cron = require("node-cron");
 const updateMatches = require("./matches");
+const updateMatches2022Season = require("./matches-2022");
+const updateMatches2020Season = require("./matches-2020");
 const updateLeaguesTeams = require("./leagues-teams");
 const ExpressError = require("../../error");
-const LEAGUE_IDS = require("../../leagues-array");
 
 // UPDATE PREMIER LEAGUE SATURDAY AND SUNDAY AT 6AM, 12PM, 4PM
-const premierLeague = cron.schedule("*	6,12,16	*	*	0,6	", async () => {
+const premierLeague = cron.schedule("0 12,4 * * 6,0", async () => {
 	try {
 		const currentDate = new Date().toLocaleString();
 		const update = await updateMatches(39);
@@ -16,8 +17,10 @@ const premierLeague = cron.schedule("*	6,12,16	*	*	0,6	", async () => {
 	}
 });
 
+
+
 // UPDATE CHAMPIONS LEAGUE NOVEMBER 23,24 AT 12PM, 3PM, 6PM
-const championsLeague = cron.schedule("*	12,15,18	23,24	11	*", async () => {
+const championsLeague = cron.schedule("0 12,15,18 23,24 11 *", async () => {
 	try {
 		const currentDate = new Date().toLocaleString();
 		const update = await updateMatches(2);
@@ -29,7 +32,7 @@ const championsLeague = cron.schedule("*	12,15,18	23,24	11	*", async () => {
 });
 
 // UPDATE EUROPA LEAGUE NOVEMBER 24,25 AT 12PM,3PM,6PM
-const europaLeague = cron.schedule("*	12,15,18	24,25	11	*", async () => {
+const europaLeague = cron.schedule("0 12,15,18 23,24 11 *", async () => {
 	try {
 		const currentDate = new Date().toLocaleString();
 		const update = await updateMatches(3);
@@ -40,60 +43,133 @@ const europaLeague = cron.schedule("*	12,15,18	24,25	11	*", async () => {
 	}
 });
 
-// const leagueCup = cron.schedule("* * 8 * * *", async () => {
-// 	try {
-// 		const currentDate = new Date().toLocaleString();
-// 		const update = await updateMatches(2);
-// 		console.log(`CRON UPDATED ENGLISH LEAGUE CUP MATCHES AT ${currentDate}`);
-// 	} catch (e) {
-// 		return new ExpressError(e);
-// 	}
-// });
+// UPDATE AT 6PM NOVEMBER 16
+const wcQualAfrica = cron.schedule("0 18 16 11 *", async () => {
+	try {
+		const currentDate = new Date().toLocaleString();
+		const update = await updateMatches2022Season(29);
+		console.log(
+			`CRON UPDATED AFRICAN WORLD CUP QUALIFICATION MATCHES AT ${currentDate}`
+		);
+		console.log(`UPDATE OBJECT: ${update}`);
+	} catch (e) {
+		return new ExpressError(e);
+	}
+});
 
-// const bundesliga = cron.schedule("* * 8 * * *", async () => {
-// 	try {
-// 		const currentDate = new Date().toLocaleString();
-// 		const update = await updateMatches(78);
-// 		console.log(`CRON UPDATED BUNDESLIGA MATCHES AT ${currentDate}`);
-// 	} catch (e) {
-// 		return new ExpressError(e);
-// 	}
-// });
+// UPDATE AT 3PM NOVEMBER 16
+const wcQualAsia = cron.schedule("0 15 16 11 *", async () => {
+	try {
+		const currentDate = new Date().toLocaleString();
+		const update = await updateMatches2022Season(30);
+		console.log(
+			`CRON UPDATED ASIAN WORLD CUP QUALIFICATION MATCHES AT ${currentDate}`
+		);
+		console.log(`UPDATE OBJECT: ${update}`);
+	} catch (e) {
+		return new ExpressError(e);
+	}
+});
 
-// const laLiga = cron.schedule("* * 8 * * *", async () => {
-// 	try {
-// 		const currentDate = new Date().toLocaleString();
-// 		const update = await updateMatches(140);
-// 		console.log(`CRON UPDATED LA LIGA MATCHES AT ${currentDate}`);
-// 	} catch (e) {
-// 		return new ExpressError(e);
-// 	}
-// });
+// UPDATE AT 10PM NOVEMBER 12
+// NEXT UPDATE SHOULD BE NOVEMBER 16 AT 11PM
+const wcQualCONCACAF = cron.schedule("0 22 12 11 *", async () => {
+	try {
+		const currentDate = new Date().toLocaleString();
+		const update = await updateMatches2022Season(31);
+		console.log(
+			`CRON UPDATED CONCACAF WORLD CUP QUALIFICATION MATCHES AT ${currentDate}`
+		);
+		console.log(`UPDATE OBJECT: ${update}`);
+	} catch (e) {
+		return new ExpressError(e);
+	}
+});
 
-// const MLS = cron.schedule("* * 8 * * *", async () => {
-// 	try {
-// 		const currentDate = new Date().toLocaleString();
-// 		const update = await updateMatches(253);
-// 		console.log(`CRON UPDATED MLS MATCHES AT ${currentDate}`);
-// 	} catch (e) {
-// 		return new ExpressError(e);
-// 	}
-// });
+// UPDATE AT 6PM NOVEMBER 12
+const wcQualEurope = cron.schedule("0 14 12 11 *", async () => {
+	try {
+		const currentDate = new Date().toLocaleString();
+		const update = await updateMatches2020Season(32);
+		console.log(
+			`CRON UPDATED EUROPEAN WORLD CUP QUALIFICATION MATCHES AT ${currentDate}`
+		);
+		console.log(`UPDATE OBJECT: ${update}`);
+	} catch (e) {
+		return new ExpressError(e);
+	}
+});
 
-// const ligueUn = cron.schedule("* * 8 * * *", async () => {
-// 	try {
-// 		const currentDate = new Date().toLocaleString();
-// 		const update = await updateMatches(61);
-// 		console.log(`CRON UPDATED LIGUE UN MATCHES AT ${currentDate}`);
-// 	} catch (e) {
-// 		return new ExpressError(e);
-// 	}
-// });
+// UPDATE AT 9PM NOVEMBER 12
+const wcQualSouthAmerica = cron.schedule("0 21 12 11 *", async () => {
+	try {
+		const currentDate = new Date().toLocaleString();
+		const update = await updateMatches2022Season(34);
+		console.log(
+			`CRON UPDATED SOUTH AMERICAN WORLD CUP QUALIFICATION MATCHES AT ${currentDate}`
+		);
+		console.log(`UPDATE OBJECT: ${update}`);
+	} catch (e) {
+		return new ExpressError(e);
+	}
+});
 
-//! ADD INTERNATIONAL COMPS
+/*
+
+const leagueCup = cron.schedule("* * 8 * * *", async () => {
+	try {
+		const currentDate = new Date().toLocaleString();
+		const update = await updateMatches(2);
+		console.log(`CRON UPDATED ENGLISH LEAGUE CUP MATCHES AT ${currentDate}`);
+	} catch (e) {
+		return new ExpressError(e);
+	}
+});
+
+const bundesliga = cron.schedule("* * 8 * * *", async () => {
+	try {
+		const currentDate = new Date().toLocaleString();
+		const update = await updateMatches(78);
+		console.log(`CRON UPDATED BUNDESLIGA MATCHES AT ${currentDate}`);
+	} catch (e) {
+		return new ExpressError(e);
+	}
+});
+
+const laLiga = cron.schedule("* * 8 * * *", async () => {
+	try {
+		const currentDate = new Date().toLocaleString();
+		const update = await updateMatches(140);
+		console.log(`CRON UPDATED LA LIGA MATCHES AT ${currentDate}`);
+	} catch (e) {
+		return new ExpressError(e);
+	}
+});
+
+const MLS = cron.schedule("* * 8 * * *", async () => {
+	try {
+		const currentDate = new Date().toLocaleString();
+		const update = await updateMatches(253);
+		console.log(`CRON UPDATED MLS MATCHES AT ${currentDate}`);
+	} catch (e) {
+		return new ExpressError(e);
+	}
+});
+
+const ligueUn = cron.schedule("* * 8 * * *", async () => {
+	try {
+		const currentDate = new Date().toLocaleString();
+		const update = await updateMatches(61);
+		console.log(`CRON UPDATED LIGUE UN MATCHES AT ${currentDate}`);
+	} catch (e) {
+		return new ExpressError(e);
+	}
+});
+
+*/
 
 // UPDATE LEAGUES TEAMS VIEW AT MIDNIGHT EVERY NIGHT
-const leaguesTeams = cron.schedule("*	0	*	*	*	", async () => {
+const leaguesTeams = cron.schedule("0 0 * * *", async () => {
 	try {
 		const currentDate = new Date().toLocaleString();
 		const update = await updateLeaguesTeams();
@@ -106,4 +182,14 @@ const leaguesTeams = cron.schedule("*	0	*	*	*	", async () => {
 premierLeague.start();
 championsLeague.start();
 europaLeague.start();
+wcQualAfrica.start();
+wcQualAsia.start();
+wcQualCONCACAF.start();
+wcQualEurope.start();
+wcQualSouthAmerica.start();
+// leagueCup.stop();
+// bundesliga.stop();
+// laLiga.stop();
+// MLS.stop();
+// ligueUn.stop();
 leaguesTeams.start();

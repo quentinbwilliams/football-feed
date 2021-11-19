eaa895d0ff6ad2a76b70e34b83bfc20c9e7be006
Quentin Williams <quentinbwilliams@gmail.com>
2021-11-19 13:22:09 -0500

    update db connection params for production branch

--- db/db.js
+++ db/db.js
@@ -1,7 +1,8 @@
 const { Client } = require("pg");
 
 const client = new Client({
-  connectionString: "postgresql:///footy_db",
+	connectionString:
+		"postgresql://doadmin:******PRIVATE*PASSWORD*****@private-matchday-db-do-user-10270224-0.b.db.ondigitalocean.com:25060/defaultdb?sslmode=require",
 });
 
 client.connect();
